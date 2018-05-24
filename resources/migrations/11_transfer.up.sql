aaf91f3ac57018d7192ebe5269a1ecaa391a79c4
Darien Hernandez <dh@hellofresh.com>
2018-05-24 12:39:27 +0200

    Fix create transfer table sql

--- resources/migrations/11_transfer.up.sql
+++ resources/migrations/11_transfer.up.sql
@@ -1,8 +1,8 @@
 -- transfer Table
 CREATE TABLE transfer (
     id UUID PRIMARY KEY NOT NULL,
-    from UUID REFERENCES bank_account(id),
-    to UUID REFERENCES bank_account(id),
+    from_account UUID REFERENCES bank_account(id),
+    to_account UUID REFERENCES bank_account(id),
     amount NUMERIC(7, 2) NOT NULL,
     date timestamp NOT NULL
 );
